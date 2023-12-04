d3503a5142fa0eb70ea2b8d2dd79ab7e7d61930c
mjamecny <jamecny.m@tutanota.com>
2023-12-04 15:03:36 +0100

    add structure, styles and handlers for open mobile menu

--- src/main.js
+++ src/main.js
@@ -1,1 +1,26 @@
-import './index.css'
+import "./index.css"
+
+const closeIcon = document.querySelector(".close")
+const hamburgerIcon = document.querySelector(".hamburger")
+const menu = document.querySelector(".menu")
+const arrowDown = document.querySelector(".arrow-down")
+const arrowUp = document.querySelector(".arrow-up")
+const submenu = document.querySelector(".submenu")
+const connect = document.querySelector(".connect")
+
+hamburgerIcon.addEventListener("click", () => {
+  hamburgerIcon.classList.add("hidden")
+  closeIcon.classList.remove("hidden")
+  menu.classList.remove("hidden")
+})
+
+closeIcon.addEventListener("click", () => {
+  hamburgerIcon.classList.remove("hidden")
+  closeIcon.classList.add("hidden")
+  menu.classList.add("hidden")
+})
+
+connect.addEventListener("click", () => {
+  submenu.classList.toggle("hidden")
+  submenu.classList.toggle("flex")
+})
